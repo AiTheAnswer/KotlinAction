ecabe7f459e4bd52cb4862b0207f754bbada4d02
Allen <15771980953@163.com>
2018-12-03 00:37:12 +0800

    新增core_net相关的代码

---
+++ core_net/src/main/java/com/allen/core_net/HttpMethod.kt
@@ -0,0 +1,11 @@
+package com.allen.core_net
+
+/**
+ * Created by Allen
+ * on 2018/12/2
+ */
+enum class HttpMethod {
+    GET,
+    POST,
+    POST_RAW
+}
---
+++ core_net/src/main/java/com/allen/core_net/RestClient.kt
@@ -0,0 +1,15 @@
+package com.allen.core_net
+
+import io.reactivex.disposables.Disposable
+
+/**
+ * Created by Allen
+ * on 2018/12/2
+ */
+class RestClient {
+    /*fun get():Disposable{
+        return request(HttpMethod.GET)
+    }*/
+
+
+}
